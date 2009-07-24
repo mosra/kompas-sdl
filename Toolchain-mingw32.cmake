d4527258abfd5888f71ddb42ac06d0fdddb9ee71
Vladimír Vondruš <mosra@centrum.cz>
2009-07-24 10:37:25 +0000

    Toolchain - kompilace pro Windows cd build && cmake -DCMAKE_TOOLCHAIN_FILE=../Toolchain-mingw32.cmake ..

---
+++ Toolchain-mingw32.cmake
@@ -0,0 +1,19 @@
+# Target OS
+SET(CMAKE_SYSTEM_NAME Windows)
+
+# C a C++ kompilátor
+SET(CMAKE_C_COMPILER i486-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER i486-mingw32-g++)
+
+# Cesta k knihovnám a programům zkompilovaným pro Windows
+SET(CMAKE_FIND_ROOT_PATH /usr/i486-mingw32 )
+
+# Tam hledat jen knihovny (kompilátor je v /usr/bin)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# Include pro SDL
+set(SDL_INCLUDE_DIR /usr/i486-mingw32/include)
+set(SDLIMAGE_INCLUDE_DIR /usr/i486-mingw32/include)
+set(SDLTTF_INCLUDE_DIR /usr/i486-mingw32/include)
