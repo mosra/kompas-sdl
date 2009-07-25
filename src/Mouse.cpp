52ada4fac2977375f1d849ca23d06726d39c67b3
Vladimír Vondruš <mosra@centrum.cz>
2009-07-25 11:39:18 +0000

    Opraveno Mouse::inArea (byl vynechán první řádek a sloupec).

--- src/Mouse.cpp
+++ src/Mouse.cpp
@@ -4,8 +4,8 @@ namespace MInterface {
 
 /* Zjištění, zda byl klik v oblasti */
 bool Mouse::inArea(int x, int y, const SDL_Rect& area) {
-    if((x > area.x && x < area.x+area.w) &&
-       (y > area.y && y < area.y+area.h))
+    if((x >= area.x && x < area.x+area.w) &&
+       (y >= area.y && y < area.y+area.h))
         return true;
     return false;
 }
