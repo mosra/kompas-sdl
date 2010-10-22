ca23b1d7e9ebbcb92edcd42632bbc08055ff16d4
Vladimír Vondruš <mosra@centrum.cz>
2010-10-22 22:58:17 +0200

    Translated doc for utilities.

--- src/utility.cpp
+++ src/utility.cpp
@@ -21,136 +21,115 @@ using namespace std;
 
 namespace Map2X { namespace Sdl {
 
-/* Doxygen neví, že std::string::iterator == string::iterator */
-#ifndef GENERATING_DOXYGEN_OUTPUT
-
-/* Pozice dalšího znaku v UTF-8 sekvenci */
 string::iterator nextUTF8Character(string* str, string::iterator position) {
-    /* Prázdný string, pozice je na konci nebo úplně v háji, konec */
     if((*str).empty() || position < (*str).begin() || position >= (*str).end())
         return (*str).end();
 
-    /* Normální ASCII znak */
+    /* Regular ASCII character */
     if((unsigned char) *position < 0x80)
         return ++position;
 
-    /* Jinak je to znak patřící do vícebajtové sekvence UTF-8. Zjistíme počet
-       bajtů UTF-8 sekvence */
+    /* Leading byte for UTF-8 sequence, get sequence length */
     int bytes = 0;
          if((unsigned char) *position >= 0xC2 && (unsigned char) *position <= 0xDF) bytes = 2;
     else if((unsigned char) *position >= 0xE0 && (unsigned char) *position <= 0xEF) bytes = 3;
     else if((unsigned char) *position >= 0xF0 && (unsigned char) *position <= 0xF4) bytes = 4;
 
-    /* Overlong encoding - začátek dvoubajtové sekvence, ale druhý znak
-       patří do normální ASCII - ještě tolerujeme */
+    /* Overlong encoding - begin of two-byte sequence, but second byte belongs
+       to ASCII - still tolerating */
     else if((unsigned char) *position == 0xC0 || (unsigned char) *position == 0xC1) {
-        /* Neočekávaný konec */
+        /* Unexpected end */
         if(++position == (*str).end()) {
-            cerr << "Chyba při zpracování UTF-8 řetězce: Overlong encoding:"
-                    << " Neočekávaný konec řetězce." << endl;
+            cerr << "UTF-8 error: overlong encoding: unexpected end of string."
+                 << endl;
             return (*str).end();
         }
 
-        /* V pořádku, vrácení pozice za těmito dvěma znaky */
-        else if((unsigned char) *position < 0x80) return ++position;
-
-        /* Druhý znak musí být z rozsahu 0 - 127! */
-        else {
-            cerr << "Chyba při zpracování UTF-8 řetězce: Overlong encoding:"
-                    << " Neplatný znak " << std::hex
-                    << (int) (unsigned char) *position << std::dec
-                    << " na pozici " << position - (*str).begin() << "." << endl;
+        /* Second byte doesn't belong to ASCII! */
+        else if((unsigned char) *position >= 0x80) {
+            cerr << "UTF-8 error: overlong encoding: invalid byte " << std::hex
+                 << (int) (unsigned char) *position << std::dec
+                 << " on position " << position - (*str).begin() << "." << endl;
             return position;
-        }
+
+        /* All OK */
+        } else return ++position;
     }
 
-    /* Rezervované hodnoty a chybové hodnoty - vrácení pozice hned dalšího
-       znaku */
+    /* Reserved and invalid values, return position of next character */
     else {
-        cerr << "Chyba při zpracování UTF-8 řetězce: Neplatný uvozovací znak "
+        cerr << "UTF-8 error: invalid leading byte of UTF-8 sequence "
              << std::hex << (int) (unsigned char) *position << std::dec
-             << " na pozici " << position - (*str).begin() << "." << endl;
+             << " on position " << position - (*str).begin() << "." << endl;
         return ++position;
     }
 
-    /* Ověření druhého a dalších bytů, zda jsou ve správném rozsahu,
-       pokud ne, vrácení pozice prvního špatného znaku */
+    /* Check whether second and next bytes are in valid range */
     for(string::const_iterator it = position+1; it != position+bytes; ++it) {
-        /* Neočekávaný konec */
+        /* Unexpected end */
         if(it == (*str).end()) {
-            cerr << "Chyba při zpracování UTF-8 řetězce: Neočekávaný konec "
-                 << "řetězce." << endl;
+            cerr << "UTF-8 error: unexpected end of string." << endl;
             return (*str).end();
         }
 
-        /* Ve správném rozsahu */
-        else if((unsigned char) *it >= 0x80 && (unsigned char) *it <= 0xBF) continue;
-
-        /* Ve špatném rozsahu */
-        cerr << "Chyba při zpracování UTF-8 řetězce: Neplatný druhý či "
-             << "další znak v UTF-8 sekvenci: " << std::hex
-             << (int) (unsigned char) *it << std::dec
-             << " na pozici " << position - (*str).begin() << "." << endl;
-        return ++position;
+        /* Wrong range */
+        else if((unsigned char) *it < 0x80 || (unsigned char) *it > 0xBF) {
+            cerr << "UTF-8 error: invalid second or next byte of UTF-8 sequence "
+                 << std::hex << (int) (unsigned char) *it << std::dec
+                 << " on position " << position - (*str).begin() << "." << endl;
+            return ++position;
+        }
     }
 
-    /* Vše prošlo v pořádku, vracíme znak za UTF-8 sekvencí */
+    /* All OK, return position of first byte after the UTF-8 sequence */
     return position+bytes;
 }
 
-/* Pozice předchozího znaku UTF-8 sekvence */
 string::iterator prevUTF8Character(string* str, string::iterator position) {
-    /* Prázdný string, pozice je úplně na začátku nebo za koncem */
     if((*str).empty() || position == (*str).begin() || position > (*str).end())
         return (*str).begin();
 
-    /* Předchozí znak je normální ASCII znak */
+    /* Previous byte is regular ASCII */
     if((unsigned char) *(position-1) < 0x80) {
-        /* Může to být ale i druhý znak u Overlong encoding */
+        /* Overlong encoding */
         if((unsigned char) *(position-2) == 0xC0 || (unsigned char) *(position-2) == 0xC1)
             return position-2;
 
         return --position;
     }
 
-    /* Předchozí znak je druhý nebo další znak UTF-8 sekvence */
-    if((unsigned char) *(position-1) >= 0x80 && (unsigned char) *(position-1) <= 0xBF) {
+    /* Invalid character */
+    if((unsigned char) *(position-1) < 0x80 || (unsigned char) *(position-1) > 0xBF) {
+        cerr << "UTF-8 error: invalid second or next byte of UTF-8 sequence "
+             << std::hex << (int) (unsigned char) *--position << std::dec
+             << " on position " << position - (*str).begin() << "." << endl;
+        return position;
 
-        /* Procházení předchozích 3 znaků, abychom zjistili délku UTF-8 sekvence */
+    /* Previous byte is second or next byte of UTF-8 sequence */
+    } else {
+        /* Foreach previous three bytes to get length of UTF-8 sequence */
         int bytes = 0;
         for(string::const_iterator it = position-1; it != position-5; --it) {
             bytes++;
 
-            /* Pokud jsme na začátku či už zde není druhý či další znak UTF-8 sekvence */
+            /* Begin of the string or there is no second or next byte of UTF-8 sequence */
             if(it == (*str).begin() || (unsigned char) *it < 0x80 || (unsigned char) *it > 0xBF) break;
         }
 
-        /* Ověření, jestli je před 1 bytem uvozovací znak dvoubytové sekvence
-           nebo před 2 byty uvozovací znak tříbytové sekvence nebo před 3 byty
-           uvozovací znak čtyřbytové sekvence */
-        if((bytes == 2 && (unsigned char) *(position-bytes) >= 0xC2 && (unsigned char) *(position-bytes) <= 0xDF) ||
-           (bytes == 3 && (unsigned char) *(position-bytes) >= 0xE0 && (unsigned char) *(position-bytes) <= 0xEF) ||
-           (bytes == 4 && (unsigned char) *(position-bytes) >= 0xF0 && (unsigned char) *(position-bytes) <= 0xF4))
-            return position-bytes;
-
-        /* Nebylo tomu tak, vrací předchozí pozici, než která byla předána */
-        else {
-            cerr << "Chyba při zpracování UTF-8 řetězce: Neplatný uvozovací znak "
+        /* Check if leading byte of UTF-8 sequence corresponds with sequence
+           length, if not, return position of previous byte */
+        if((bytes == 2 && ((unsigned char) *(position-bytes) < 0xC2 || (unsigned char) *(position-bytes) > 0xDF)) ||
+           (bytes == 3 && ((unsigned char) *(position-bytes) < 0xE0 || (unsigned char) *(position-bytes) > 0xEF)) ||
+           (bytes == 4 && ((unsigned char) *(position-bytes) < 0xF0 || (unsigned char) *(position-bytes) > 0xF4))) {
+            cerr << "UTF-8 error: invalid second or next byte of UTF-8 sequence "
                  << std::hex << (int) (unsigned char) *(position-bytes)
-                 << std::dec << " u " << bytes << "-bytové sekvence"
-                 << " na pozici " << position - (*str).begin() << "." << endl;
+                 << std::dec << " in " << bytes << "-byte sequence on position "
+                 << position - (*str).begin() << "." << endl;
             return position-1;
-        }
+
+        /* All OK, return position of leading byte of previous UTF-8 sequence */
+        } else return position-bytes;
     }
-
-    /* Jiný znak - chyba! */
-    cerr << "Chyba při zpracování UTF-8 řetězce: Neplatný druhý či "
-         << "další znak v UTF-8 sekvenci: " << std::hex
-         << (int) (unsigned char) *--position << std::dec
-         << " na pozici " << position - (*str).begin() << "." << endl;
-    return position;
 }
-
-#endif
 
 }}
--- src/utility.h
+++ src/utility.h
@@ -15,64 +15,57 @@
     GNU Lesser General Public License version 3 for more details.
 */
 
-/**
- * @file utility.h
- * @brief Utility nezávislé na SDL sloužící k triviálním operacím nad C++ knihovnami
+/** @file
+ * @brief Basic utilities
  */
 
 #include <string>
 
-/**
- * @brief Nízkoúrovňové věci
- *
- * Funkce a třídy nezávislé na žádné externí knihovně, pro správu konfigurace,
- * lokalizace atd.
- */
 namespace Map2X { namespace Sdl {
 
 /**
- * @brief Zarovnání
+ * @brief Align
  *
- * Slouží ke specifikaci zarovnání objektu (spojením vertikální a horizontální
- * hodnoty pomocí binárního OR).
+ * Specifies align of an object. Combine values with binary OR.
  */
 enum Align {
-    ALIGN_DEFAULT = 0,      /**< @brief Default zarovnání (vlevo nahoru) */
-    ALIGN_LEFT = 0x01,      /**< @brief Doleva */
-    ALIGN_CENTER = 0x02,    /**< @brief Doprostřed (horizontálně) */
-    ALIGN_RIGHT  = 0x04,    /**< @brief Doprava */
-    ALIGN_TOP  = 0x10,      /**< @brief Nahoru */
-    ALIGN_MIDDLE = 0x20,    /**< @brief Doprostřed (vertikálně) */
-    ALIGN_BOTTOM = 0x40     /**< @brief Dolů */
+    ALIGN_DEFAULT = 0,      /**< @brief Default align (top left) */
+    ALIGN_LEFT = 0x01,      /**< @brief Left */
+    ALIGN_CENTER = 0x02,    /**< @brief Center (horizontally) */
+    ALIGN_RIGHT  = 0x04,    /**< @brief Right */
+    ALIGN_TOP  = 0x10,      /**< @brief Top */
+    ALIGN_MIDDLE = 0x20,    /**< @brief Middle (vertically) */
+    ALIGN_BOTTOM = 0x40     /**< @brief Bottom */
 };
 
 /**
- * @brief Nalezení pozice dalšího znaku v UTF-8 řetězci
+ * @brief Look up position of next character in UTF-8 string
+ * @param str           UTF-8 string
+ * @param position      Current position
+ * @return Position of next UTF-8 character (if everything went fine), or:
+ *  - If current byte is invalid, prints error to stderr and returns position
+ *    of next byte.
+ *  - If any of next byte is invalid, prints error to stderr and returns
+ *    position of first invalid byte.
+ *  - If the string has unexpected end or the position is after end, returns
+ *    one position after end.
  *
- * Pokud je na aktuální pozici neplatný uvozovací znak UTF-8 sekvence, vrací
- * s chybou pozici následujícího znaku, pokud nesouhlasí hodnota druhého či
- * dalšího znaku UTF-8 sekvence, vrací pozici prvního znaku, na kterém nastala
- * chyba. Pokud je řetězec neočekávaně ukončen nebo předaná pozice odpovídá
- * poslednímu znaku, vrací pozici za koncem.
  * @sa http://en.wikipedia.org/wiki/UTF-8#Description
- * @param   str         String
- * @param   position    Startovní pozice
- * @return  Pozice dalšího znaku
  * @todo Proč nejde const string&? a const_iterator?
  */
 std::string::iterator nextUTF8Character(std::string* str, std::string::iterator position);
 
 /**
- * @brief Nalezení pozice předchozího znaku v UTF-8 řetězci
+ * @brief Look up position of previous character in UTF-8 string
+ * @param str           UTF-8 string
+ * @param position      Current position
+ * @return  Position of previous UTF-8 character (if everything went fine), or:
+ *  - If previous byte is invalid, prints error to stderr and returns that
+ *    position.
+ *  - If leading byte of UTF-8 sequence is invalid, returns position of
+ *    previous byte.
  *
- * Pokud je na předchozí pozici neplatný druhý či další znak UTF-8 sekvence,
- * vrací s chybou tuto pozici, pokud je před těmito znaky neplatný uvozovací
- * znak (jeho hodnota se neshoduje s délkou sekvence či jiná chyba), vrací
- * předchozí pozici, než která byla předána.
  * @sa http://en.wikipedia.org/wiki/UTF-8#Description
- * @param   str         String
- * @param   position    Startovní pozice
- * @return  Pozice předchozího znaku
  * @todo Proč nejde const string&? a const_iterator?
  */
 std::string::iterator prevUTF8Character(std::string* str, std::string::iterator position);
